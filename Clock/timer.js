4dc8226e2d5a8218285ec0b91ee73114a29ebcfb
Praythecube <pray.lav@gmail.com>
2022-08-10 23:06:36 +0900

    Hands move.
    Hand go move.

---
+++ Clock/timer.js
@@ -0,0 +1,33 @@
+let hours;
+let minutes;
+let seconds;
+
+const hour_hand = document.getElementById("hour-hand");
+const minute_hand = document.getElementById("minute-hand");
+const second_hand = document.getElementById("second-hand");
+
+function calc() {
+    var time = new Date();
+
+    hours = time.getHours();
+    minutes = time.getMinutes();
+    seconds = time.getSeconds();
+
+    const secDeg = (seconds / 60) * 360;
+    second_hand.style.transform = `rotate(${secDeg}deg)`;
+
+    const minDeg = ((minutes / 60) * 360) + ((seconds/60) * 6) - 90;
+    minute_hand.style.transform = `rotate(${minDeg}deg)`;
+
+    const hourDeg = (((hours / 12) * 360) / 2) - 90;
+    hour_hand.style.transform = `rotate(${hourDeg}deg)`;
+
+    console.log(hourDeg);
+
+
+}
+
+setInterval(calc, 1000);
+
+calc();
+
